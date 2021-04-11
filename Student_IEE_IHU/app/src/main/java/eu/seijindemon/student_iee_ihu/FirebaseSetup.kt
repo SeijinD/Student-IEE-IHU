27276824da3639173e4bc4cd2312036087f3f029
SeijinD <seijindemon@protonmail.com>
2021-04-11 17:46:38 +0300

    change FirebaseSetup and add fonts

--- Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/FirebaseSetup.kt
+++ Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/FirebaseSetup.kt
@@ -1,6 +1,7 @@
 package eu.seijindemon.student_iee_ihu
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -8,11 +9,12 @@ import com.google.firebase.storage.StorageReference
 
 class FirebaseSetup {
 
-    lateinit var auth: FirebaseAuth
-    lateinit var userReference: DatabaseReference
-    lateinit var database: FirebaseDatabase
-    lateinit var storage: FirebaseStorage
-    lateinit var userStorage: StorageReference
+    var auth: FirebaseAuth? = null
+    var usersReference: DatabaseReference? = null
+    var userReference: DatabaseReference? = null
+    var database: FirebaseDatabase? = null
+    var storage: FirebaseStorage? = null
+    var userStorage: StorageReference? = null
 
     fun setupFirebase()
     {
@@ -22,11 +24,15 @@ class FirebaseSetup {
         storage = FirebaseStorage.getInstance()
 
         // Realtime Database
-        userReference = database.reference.child("profile")
+        usersReference = database!!.reference.child("profile")
+        if (auth?.currentUser != null)
+        {
+            userReference = usersReference?.child(auth?.currentUser!!.uid)
+        }
 
 
         // Storage
-        userStorage = storage.reference.child("users_images")
+        userStorage = storage!!.reference.child("users_images")
 
     }
 
