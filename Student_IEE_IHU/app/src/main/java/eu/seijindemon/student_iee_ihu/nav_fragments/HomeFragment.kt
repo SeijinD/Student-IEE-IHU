6b2910e13d81c1fcc9aaa9a44c3042e6fc727dd1
SeijinD <seijindemon@protonmail.com>
2021-04-11 17:49:14 +0300

    add fragment home and settings

---
+++ Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/nav_fragments/HomeFragment.kt
@@ -0,0 +1,28 @@
+package eu.seijindemon.student_iee_ihu.nav_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eu.seijindemon.student_iee_ihu.R
+
+class HomeFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return view
+    }
+
+
+}
