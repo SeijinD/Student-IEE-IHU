156f34809587eca970471b691cae1a3c3f9f3e5a
SeijinD <seijindemon@protonmail.com>
2021-05-02 23:24:20 +0300

    Add Community page and change Home page

---
+++ Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/nav_fragments/CommunityFragment.kt
@@ -0,0 +1,23 @@
+package eu.seijindemon.student_iee_ihu.nav_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eu.seijindemon.student_iee_ihu.R
+
+class CommunityFragment : Fragment() {
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_community, container, false)
+
+
+
+        return view
+    }
+
+
+}
