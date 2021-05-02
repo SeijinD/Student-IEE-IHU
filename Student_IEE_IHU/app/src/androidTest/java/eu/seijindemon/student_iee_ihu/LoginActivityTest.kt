53075d175702e9a7e87bad45d6504bc000d1f3bf
SeijinD <seijindemon@protonmail.com>
2021-05-02 23:25:17 +0300

    add login test

---
+++ Student_IEE_IHU/app/src/androidTest/java/eu/seijindemon/student_iee_ihu/LoginActivityTest.kt
@@ -0,0 +1,28 @@
+package eu.seijindemon.student_iee_ihu
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class LoginActivityTest {
+    @Rule @JvmField
+    var activityRule = ActivityTestRule(LoginActivity::class.java)
+
+    @Test
+    fun login() {
+        onView(withId(R.id.login_email)).perform(replaceText("georgekara2010@yahoo.gr"))
+        onView(withId(R.id.login_password)).perform(replaceText("password"))
+        onView(withId(R.id.login_button)).perform(click())
+    }
+
+}
