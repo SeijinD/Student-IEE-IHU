093c2548f9db054894746a8852c6f6827bf81c74
SeijinD <seijindemon@protonmail.com>
2021-05-03 10:16:11 +0300

    add community pages in Community Page, Grapch, strings, icons etc

--- Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/nav_fragments/CommunityFragment.kt
+++ Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/nav_fragments/CommunityFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import eu.seijindemon.student_iee_ihu.R
+import kotlinx.android.synthetic.main.fragment_community.view.*
 
 class CommunityFragment : Fragment() {
-
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_community, container, false)
 
+        view.discords.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.menuDiscords)
+        }
 
+        view.facebook_groups.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.menuFacebookGroups)
+        }
+
+        view.facebook_pages.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.menuFacebookPages)
+        }
+
+        view.other_community.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.menuOtherCommunity)
+        }
 
         return view
     }
---
+++ Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/nav_fragments/community_fragments/FacebookPagesFragment.kt
@@ -0,0 +1,23 @@
+package eu.seijindemon.student_iee_ihu.nav_fragments.community_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eu.seijindemon.student_iee_ihu.R
+
+class FacebookPagesFragment : Fragment() {
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_facebook_pages, container, false)
+
+
+
+        return view
+    }
+
+
+}
