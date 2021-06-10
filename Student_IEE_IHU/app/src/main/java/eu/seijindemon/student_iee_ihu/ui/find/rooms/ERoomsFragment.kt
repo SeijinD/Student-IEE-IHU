f2e047d0aa416beef9d121050e6bb2ba53f7ee60
SeijinD <seijindemon@protonmail.com>
2021-06-11 00:16:56 +0300

    add rrooms and erooms in zoom rooms with viewpage2

---
+++ Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/ui/find/rooms/ERoomsFragment.kt
@@ -0,0 +1,23 @@
+package eu.seijindemon.student_iee_ihu.ui.find.rooms
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import eu.seijindemon.student_iee_ihu.R
+
+class ERoomsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_erooms, container, false)
+
+
+
+        return view
+    }
+
+
+}
