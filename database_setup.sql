441b7ce7ea0ac512970a545c8bb00620684aa3a2
SeijinD <seijindemon@protonmail.com>
2021-07-07 15:39:41 +0300

    database setup

---
+++ database_setup.sql
@@ -0,0 +1,128 @@
+-- Courses
+
+CREATE TABLE `courses` (
+  `id` int(11) NOT NULL,
+  `title` VARCHAR(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `semester` VARCHAR(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `teachers` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=UTF8_UNICODE_CI;
+
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Teachers
+
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `personal_site` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `teachers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Communities
+
+CREATE TABLE `communities` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `communities`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `communities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Offers
+
+CREATE TABLE `offers` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `offers`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `offers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Maps
+
+CREATE TABLE `maps` (
+  `id` int(11) NOT NULL,
+  `latitude` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `longitude` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `maps`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `maps`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Official Services
+
+CREATE TABLE `official_services` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `official_services`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `official_services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Unofficial Services
+
+CREATE TABLE `unofficial_services` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `creator` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `unofficial_services`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `unofficial_services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- Useful Website
+
+CREATE TABLE `useful_websites` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `useful_websites`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `useful_websites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
