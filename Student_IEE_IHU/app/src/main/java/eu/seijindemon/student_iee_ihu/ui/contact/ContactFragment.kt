8b41590ad736a2a562bd4cd525560415446ffa8d
SeijinD <seijindemon@protonmail.com>
2021-07-09 18:35:22 +0300

    update contact page

--- Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/ui/contact/ContactFragment.kt
+++ Student_IEE_IHU/app/src/main/java/eu/seijindemon/student_iee_ihu/ui/contact/ContactFragment.kt
@@ -1,5 +1,7 @@
 package eu.seijindemon.student_iee_ihu.ui.contact
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,6 +20,84 @@ class ContactFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_contact, container, false)
+
+        view.first_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013621")))
+        }
+        view.second_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013604")))
+        }
+        view.third_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013123")))
+        }
+        view.fourth_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013709")))
+        }
+        view.fourth_contact_number_2_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013712")))
+        }
+        view.fifth_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013790")))
+        }
+        view.sixth_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013667")))
+        }
+        view.sixth_contact_number_2_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013124")))
+        }
+        view.seventh_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013148")))
+        }
+        view.seventh_contact_number_2_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013139")))
+        }
+        view.eighth_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2152157850")))
+        }
+        view.ninth_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013620")))
+        }
+        view.tenth_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013592")))
+        }
+        view.eleventh_contact_number_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "2310013665")))
+        }
+
+        view.first_contact_email_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "info@iee.ihu.gr", null)))
+        }
+        view.second_contact_email_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "noc@teithe.gr", null)))
+        }
+        view.third_contact_email_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "dobros@admin.teithe.gr", null)))
+        }
+        view.fourth_contact_email_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "erasmus.admin@the.ihu.gr", null)))
+        }
+        view.fifth_contact_email_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "praktiki.espa@teithe.gr", null)))
+        }
+        view.sixth_contact_email_1_button.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "merimna@the.ihu.gr", null)))
+        }
+//        view.seventh_contact_email_1_button.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "", null)))
+//        }
+//        view.eighth_contact_email_1_button.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "", null)))
+//        }
+//        view.ninth_contact_email_1_button.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "", null)))
+//        }
+//        view.tenth_contact_email_1_button.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "", null)))
+//        }
+//        view.eleventh_contact_email_1_button.setOnClickListener {
+//            startActivity(Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "", null)))
+//        }
+
 
 
 
